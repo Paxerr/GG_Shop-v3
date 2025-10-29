e8051e2f06d06d4d8a1fc8df72e333dcea2c2189
paxerr <quyetnguyenst2005@gmail.com>
2025-10-29 19:40:55 +0700

    Add project files.

---
+++ GG_Shop v3/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GG_Shop_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult index_admin()
+        {
+            ViewBag.Message = "admin";
+
+            return View();
+        }
+        public ActionResult listCustomer()
+        {
+            ViewBag.Message = "listCustomer";
+
+            return View();
+        }
+        public ActionResult addCustomer()
+        {
+            ViewBag.Message = "addCustomer";
+
+            return View();
+        }
+        public ActionResult listUsers()
+        {
+            ViewBag.Message = "listUsers";
+
+            return View();
+        }
+        public ActionResult addUsers()
+        {
+            ViewBag.Message = "addUsers";
+
+            return View();
+        }
+        public ActionResult AddProduct()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult ListProduct()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ GG_Shop v3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GG_Shop_v3.MvcApplication" Language="C#" %>
