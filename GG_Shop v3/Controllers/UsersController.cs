6250c05b57cf2ed315a716783c4d26cb6ac8df0c
paxerr <quyetnguyenst2005@gmail.com>
2025-11-19 18:53:19 +0700

    sua lai theo ajax

--- GG_Shop v3/Controllers/UsersController.cs
+++ GG_Shop v3/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using GG_Shop_v3.Models;
 
 namespace GG_Shop_v3.Controllers
@@ -38,40 +39,61 @@ namespace GG_Shop_v3.Controllers
         // GET: Users/Create
         public ActionResult Create()
         {
-            ViewBag.RankList = new SelectList(new List<string> { "Vip", "Thường"});
-            ViewBag.RoleList = new SelectList(new List<string> { "Admin", "Khách hàng"});
             return View();
         }
 
-        // POST: Users/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Username,Email,Password,Full_Name,Phone_Number,Country,Orders,Rank,Total_Spent,Role")] User user)
+        public string Insert()
         {
-            if (db.users.Any(u => u.Username == user.Username))
+            //lấy về mssv.
+            string rs = "";
+            string username_str = Request["username"];
+            string email_str = Request["email"];
+            string password_str = Request["password"];
+            string full_name_str = Request["full_name"];
+            string phone_number_str = Request["phone_number"];
+            string country_str = Request["country"];
+            string orders_str = Request["orders"];
+            string rank_str = Request["slc_rank"];
+            string total_spent_str = Request["total_spent"];
+            string role_str = Request["slc_role"];
+
+            int orders;
+            int.TryParse(orders_str, out orders);
+            double total_spent;
+            double.TryParse(total_spent_str, out total_spent);
+
+            //kiểm tra xem đã tồn tại đối tượng sv có mssv này chưa
+            if (db.users.Any(o => o.Username == username_str))
             {
-                ModelState.AddModelError("Username", "Tên tài khoản đã tồn tại");
+                rs = "Tên đăng nhập đã tồn tại";
             }
-            if (db.users.Any(u => u.Email == user.Email))
+            else
             {
-                ModelState.AddModelError("Email", "Email đã tồn tại");
+                //chưa tồn tại teen ddawng nhap.
+
+
+
+                //update thông tin cho đối tượng
+                User user = new User(username_str, email_str, password_str, full_name_str, phone_number_str, country_str, orders, rank_str, total_spent, role_str);
+
+                try
+                {
+                    db.users.Add(user);
+                    db.SaveChanges();
+                    rs = "Thêm người dùng thành công";
+                }
+                catch (Exception ex)
+                {
+                    rs = "Thêm người dùng thất bại";
+                }
             }
-            if (user.Orders < 0)
-            {
-                ModelState.AddModelError("Orders", "Số đơn hàng phải >= 0");
-            }
-            if (ModelState.IsValid)
-            {
-                db.users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.RankList = new SelectList(new List<string> { "Vip", "Thường" });
-            ViewBag.RoleList = new SelectList(new List<string> { "Admin", "Khách hàng" });
-            return View(user);
+
+            return rs;
         }
+
+
+
 
         // GET: Users/Edit/5
         public ActionResult Edit(int? id)
